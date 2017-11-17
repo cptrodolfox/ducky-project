88bc4371120efae324df5cab9ed41c23ecaad089
Paul Silva <paul.silva@yachaytech.edu.ec>
2017-11-17 08:49:39 -0500

    Update default.nix
    Added the dependencies necessary to use threepenny-gui

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { mkDerivation, acid-state, attoparsec, base, bytestring
-, containers, mtl, safecopy, stdenv, text, time
-}:
+, containers, mtl, safecopy, stdenv, text, time, haddock, snap
+, threepenny-gui }:
 mkDerivation {
   pname = "the-project";
   version = "0.1.0.0";
@@ -9,7 +9,7 @@ mkDerivation {
   isExecutable = true;
   executableHaskellDepends = [
     acid-state attoparsec base bytestring containers mtl safecopy text
-    time
+    time haddock snap threepenny-gui
   ];
   description = "A university project management system [small]";
   license = stdenv.lib.licenses.bsd3;
