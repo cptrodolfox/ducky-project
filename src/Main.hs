02e7213d6be386e0c78a4f8b827b603772a5edd1
Paul Silva <paul.silva@yachaytech.edu.ec>
2017-11-17 23:49:50 -0500

    Update Main.hs
    Added the add student functionality plus improvements on the GUI part

--- src/Main.hs
+++ src/Main.hs
@@ -67,10 +67,8 @@ setup state window = void $ do
 
   header <- UI.h2 # set UI.text "Welcome to the Project Management Utility of Yachay Tech" 
   buttonAdd <- UI.button # set UI.text "Add Project"
-  -- buttonRemove <- UI.button # set UI.text "Remove Project"
   buttonShStudent <- UI.button # set UI.text "Show Students"
-  -- buttonAddStudent <- UI.button # set UI.text "Add Student"
-  -- buttonRmStudent <- UI.button # set UI.text "Remove Student"
+  buttonAddStudent <- UI.button # set UI.text "Add Student"
   
   -- | Titles for the Project Table
   title <- UI.h4 # set UI.text "Title" # set style [("border","solid black 1px")]
@@ -78,19 +76,22 @@ setup state window = void $ do
   budget <- UI.h4 # set UI.text "Budget" # set style [("border","solid black 1px")]
   participants <- UI.h4 # set UI.text "Participants" # set style [("border","solid black 1px")]
   let proj = unsafePerformIO $ query state GetProjects
+      elemProj = Map.elems proj
       prTitle = map string $ map ByteString.unpack $ map project_title $ Map.elems proj 
       prDescription = map string $ map ByteString.unpack $ map project_description $ Map.elems proj 
       prBudget = map string $ map show $ map project_budget $ Map.elems proj
-      prStudent = map string $ map show $ map project_participants $ Map.elems proj 
+      idParticipant = [ student_id x | x <- concat $ map project_participants elemProj]
+      prStudent = map string $ map show idParticipant
   
-  canvas <- UI.div #+ [element header, 
-                       column [ grid [ map element [title, description, budget, participants], 
-                                       map column [prTitle , prDescription, prBudget, prStudent]] # set style [("border","solid black 1px")]],
-                       grid [map element [buttonAdd, buttonShStudent]]]
+  canvas <- UI.div #+ [ 
+                       column [ element header, 
+                                grid [ map element [title, description, budget, participants], 
+                                       map column [prTitle , prDescription, prBudget, prStudent]],
+                                grid [ map element [buttonAdd, buttonShStudent, buttonAddStudent]] # set style [("position","absolute"),("left","50%"),("transform","translateX(-50%)")]]
+                       ]
   
   getBody window 
-    #+ [ element canvas ]
-    # set style [("background-color", "#eeeeee"),("vertical-align","middle"),("horizontal-align","middle")]
+    #+ [ element canvas # set style [("position","absolute"),("background-color", "#eeeeee"),("margin","auto"),("left","50%"),("transform","translateX(-50%)")]] # set style [("position","relative")]
     
   on UI.click buttonShStudent $ \_ -> void $ do
     -- | Students Section
@@ -98,10 +99,10 @@ setup state window = void $ do
     lName <- UI.h4 # set UI.text "Last Name" # set style [("border","solid black 1px")]
     idS <- UI.h4 # set UI.text "Id" # set style [("border","solid black 1px")]
     let stud = unsafePerformIO $ query state GetStudents
-        fromPerson = map person $ Map.elems stud
-        stFName = map string $ map ByteString.unpack $ map fname $ fromPerson
-        stLName = map string $ map ByteString.unpack $ map lname $ fromPerson
-        stId = map string $ map show $ map id $ fromPerson
+        people = map person $ Map.elems stud
+        stFName = map string $ map ByteString.unpack $ map fname $ people
+        stLName = map string $ map ByteString.unpack $ map lname $ people
+        stId = map string $ map show $ map id $ people
     
     header <- UI.h2 #+ [string $ "Students"] 
     
@@ -136,7 +137,7 @@ setup state window = void $ do
           projPartId = read projParticipants :: Id
           projPart = PStudent projPartId
           nextKey = let listId = Map.keys proj 
-                  in if length listId > 1 then maximum listId + 1 else 1
+                  in if length listId > 0 then maximum listId + 1 else 1
           project = Project (ByteString.pack projTitle) (ByteString.pack projDescription) projBudN emptyResultsId [projPart] 
       liftIO $ update state $ AddProject nextKey project
     
@@ -150,35 +151,57 @@ setup state window = void $ do
       # set style [("align","center")]
     
     getBody window # set children [ projCanvas , add ]
+    
+  on UI.click buttonAddStudent $ \_ -> do
+    -- | UI input elements for student 
+    studFNameInput <- UI.input # set (attr "placeholder") "Student's first name"
+    studLNameInput <- UI.input # set (attr "placeholder") "Student's last name"
+    studBDInput <- UI.input # set (attr "placeholder") "day"
+    studBMInput <- UI.input # set (attr "placeholder") "month"
+    studBYInput <- UI.input # set (attr "placeholder") "year"
+    studGenderInput <- UI.input # set (attr "placeholder") "Male | Female | Other"
+    studPIInput <- UI.input # set (attr "placeholder") "True | False"
+    studIdInput <- UI.input # set (attr "placeholder") "Student's Id"
+    
+    add <- UI.button # set UI.text "Add"
+    
+    on UI.click add $ \_ -> void $ do
+      -- | Values of the new Project
+      studFName <- get value studFNameInput
+      studLName <- get value studLNameInput
+      studBD <- get value studBDInput
+      studBM <- get value studBMInput
+      studBY <- get value studBYInput
+      studGender <- get value studGenderInput
+      studPI <- get value studPIInput
+      studId <- get value studIdInput
+      let year = read studBY :: Integer
+          month = read studBM :: Int
+          day = read studBD :: Int
+          birthday = fromGregorian year month day
+          studIdN = read studId :: Integer          
+          std_person = Person (ByteString.pack studFName) (ByteString.pack studLName) birthday (toBool studPI) (toGender studGender) studIdN
+          std = Student std_person
+      liftIO $ update state $ AddStudent std
+    
+    header <- UI.h2 #+ [string $ "Add a new Student"] 
+    projCanvas <- UI.div #. "small-4 row" 
+      #+ [ element header, 
+           row [ grid [[string "First name:", element studFNameInput]
+          , [string "Last name:", element studLNameInput]
+          , [string "Birthday:", grid [map element [studBYInput, studBMInput, studBDInput]]]
+          , [string "Gender:", element studGenderInput]
+          , [string "PI:", element studPIInput]
+          , [string "Id:", element studIdInput]]]] 
+      # set style [("align","center")]
+    
+    getBody window # set children [ projCanvas , add ]
   
-  --on UI.click buttonRemove $ \_ -> do
-  --on UI.click buttonAddStudent $ \_ -> do    
-  
-  {- putStrLn "The name of the university is test"
-  state <- openLocalState $ createUniversity $ pack "test"
-  allStuds <- query state GetStudents
-  mapM_ print allStuds
-  putStrLn "Please write the id number of a new student: "
-  idS <- getLine
-  let idN = read (unpack idS) :: Integer
-  putStrLn "Please write the student's first name: "
-  fname <- getLine
-  putStrLn "Please write the student's last name: "
-  lname <- getLine
-  putStrLn "Please write the student's birthday first year, second month and third day"
-  yS <- getLine
-  mS <- getLine
-  dS <- getLine
-  let year = read (unpack yS) :: Integer
-      month = read (unpack mS) :: Int
-      day = read (unpack dS) :: Int
-      birthday = fromGregorian year month day
-  putStrLn "The student's birthday is: "
-  putStrLn $ showGregorian birthday
-  putStrLn "For testing the student's genre will be other."
-  let std_person = Person fname lname birthday False Other idN
-      std = Student std_person
-  putStrLn $ show std
-  update state (AddStudent $ std)
-  putStrLn "Ending session..."
-  return ()-}
+toBool :: String -> Bool
+toBool "True" = True
+toBool "False" = False
+
+toGender :: String -> Genre
+toGender "Male" = Male
+toGender "Female" = Female
+toGender "Other" = Other 
